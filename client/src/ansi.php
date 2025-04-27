4b216db72fc04d59ed8405bde5a649d6e066666c
1Hyena <hyena@hyena.net.ee>
2025-04-27 21:49:14 +0300

    improved ansi.css generation logic

--- client/src/ansi.php
+++ client/src/ansi.php
@@ -1,28 +1,107 @@
 <?php
 
-$colors = array(
-    ":not(.ans-b).ans-fg-black" => array(46, 52, 54),
-    ":not(.ans-b).ans-fg-red" => array(204, 0, 0),
-    ":not(.ans-b).ans-fg-green" => array(78, 154, 6),
-    ":not(.ans-b).ans-fg-yellow" => array(196, 160, 0),
-    ":not(.ans-b).ans-fg-blue" => array(52, 101, 164),
-    ":not(.ans-b).ans-fg-magenta" => array(117, 80, 123),
-    ":not(.ans-b).ans-fg-cyan" => array(6, 152, 154),
-    ":not(.ans-b).ans-fg-white" => array(211, 215, 207),
-    ".ans-b.ans-fg-black" => array(85, 87, 83),
-    ".ans-b.ans-fg-red" => array(239, 41, 41),
-    ".ans-b.ans-fg-green" => array(138, 226, 52),
-    ".ans-b.ans-fg-yellow" => array(252, 233, 79),
-    ".ans-b.ans-fg-blue" => array(114, 159, 207),
-    ".ans-b.ans-fg-magenta" => array(173, 127, 168),
-    ".ans-b.ans-fg-cyan" => array(52, 226, 226),
-    ".ans-b.ans-fg-white" => array(238, 238, 236)
+$theme = "Arion";
+
+$themes = array(
+    "Arion" => array(
+        "black"         => "#000000",
+        "red"           => "#800000",
+        "green"         => "#008000",
+        "yellow"        => "#808000",
+        "blue"          => "#306090",
+        "magenta"       => "#800080",
+        "cyan"          => "#008080",
+        "white"         => "#aaaaaa",
+        "hi-black"      => "#555555",
+        "hi-red"        => "#ff0000",
+        "hi-green"      => "#00ff00",
+        "hi-yellow"     => "#ffff00",
+        "hi-blue"       => "#0080ff",
+        "hi-magenta"    => "#ff00ff",
+        "hi-cyan"       => "#00ffff",
+        "hi-white"      => "#ffffff",
+        "bg-color"      => "#000000",
+        "fg-color"      => "#aaaaaa"
+    ),
+    "ANSI" => array(
+        "black"         => "#000000",
+        "red"           => "#800000",
+        "green"         => "#008000",
+        "yellow"        => "#808000",
+        "blue"          => "#000080",
+        "magenta"       => "#800080",
+        "cyan"          => "#008080",
+        "white"         => "#c0c0c0",
+        "hi-black"      => "#808080",
+        "hi-red"        => "#ff0000",
+        "hi-green"      => "#00ff00",
+        "hi-yellow"     => "#ffff00",
+        "hi-blue"       => "#0000ff",
+        "hi-magenta"    => "#ff00ff",
+        "hi-cyan"       => "#00ffff",
+        "hi-white"      => "#ffffff",
+        "bg-color"      => "#000000",
+        "fg-color"      => "#c0c0c0"
+    ),
+    "Aardvark Blue" => array(
+        "black"         => "#191919",
+        "red"           => "#aa342e",
+        "green"         => "#4b8c0f",
+        "yellow"        => "#dbba00",
+        "blue"          => "#1370d3",
+        "magenta"       => "#c43ac3",
+        "cyan"          => "#008eb0",
+        "white"         => "#bebebe",
+        "hi-black"      => "#454545",
+        "hi-red"        => "#f05b50",
+        "hi-green"      => "#95dc55",
+        "hi-yellow"     => "#ffe763",
+        "hi-blue"       => "#60a4ec",
+        "hi-magenta"    => "#e26be2",
+        "hi-cyan"       => "#60b6cb",
+        "hi-white"      => "#f7f7f7",
+        "bg-color"      => "#102040",
+        "fg-color"      => "#dddddd"
+    )
 );
+
+$colors = array(
+    ":not(.ans-b).ans-fg-black"     => "black",
+    ":not(.ans-b).ans-fg-red"       => "red",
+    ":not(.ans-b).ans-fg-green"     => "green",
+    ":not(.ans-b).ans-fg-yellow"    => "yellow",
+    ":not(.ans-b).ans-fg-blue"      => "blue",
+    ":not(.ans-b).ans-fg-magenta"   => "magenta",
+    ":not(.ans-b).ans-fg-cyan"      => "cyan",
+    ":not(.ans-b).ans-fg-white"     => "white",
+    ".ans-b.ans-fg-black"           => "hi-black",
+    ".ans-b.ans-fg-red"             => "hi-red",
+    ".ans-b.ans-fg-green"           => "hi-green",
+    ".ans-b.ans-fg-yellow"          => "hi-yellow",
+    ".ans-b.ans-fg-blue"            => "hi-blue",
+    ".ans-b.ans-fg-magenta"         => "hi-magenta",
+    ".ans-b.ans-fg-cyan"            => "hi-cyan",
+    ".ans-b.ans-fg-white"           => "hi-white"
+);
+
+foreach ($colors as $c => $v) {
+    if (!is_string($v) || !array_key_exists($v, $themes[$theme])) {
+        continue;
+    }
+
+    $hex = $themes[$theme][$v];
+    list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x");
+
+    $colors[$c] = array(
+        "rgb" => array(intval($r), intval($g), intval($b)),
+        "lerp" => strcasecmp($themes[$theme]["bg-color"], $hex) ? true : false
+    );
+}
 
 $styles = array(
     ".ans-default"         => array(
-        "color"            => "#D3D7CF",
-        "background-color" => "#2E3436"
+        "color"            => $themes[$theme]["fg-color"],
+        "background-color" => $themes[$theme]["bg-color"]
     ),
     ".ans-italic"          => array("font-style"       => "italic"         ),
     ".ans-underline"       => array("text-decoration"  => "underline"      ),
@@ -33,7 +112,7 @@ $styles = array(
 );
 
 foreach ($colors as $c => $v) {
-    $code = sprintf("#%02X%02X%02X",$v[0], $v[1], $v[2]);
+    $code = sprintf("#%02X%02X%02X",$v["rgb"][0], $v["rgb"][1], $v["rgb"][2]);
     $class = $c;
 
     echo(
@@ -57,11 +136,16 @@ foreach ($styles as $class => $properties) {
 
 echo(".ans-lerp {\n");
 
-foreach ($colors as $c1 => $v1) {
-    foreach ($colors as $c2 => $v2) {
-        if ($c1 === $c2) {
+foreach ($colors as $c1 => $c1v) {
+    foreach ($colors as $c2 => $c2v) {
+        if ($c1 === $c2
+        || !$c1v["lerp"]
+        || !$c2v["lerp"]) {
             continue;
         }
+
+        $v1 = $c1v["rgb"];
+        $v2 = $c2v["rgb"];
 
         $v = array(
             floor(($v1[0] + $v2[0]) / 2),
@@ -93,12 +177,20 @@ foreach ($colors as $c1 => $v1) {
     }
 }
 
-foreach ($colors as $c1 => $v1) {
-    foreach ($colors as $c2 => $v2) {
-        foreach ($colors as $c3 => $v3) {
+foreach ($colors as $c1 => $c1v) {
+    foreach ($colors as $c2 => $c2v) {
+        foreach ($colors as $c3 => $c3v) {
             if ($c1 === $c2 || $c2 === $c3) {
                 continue;
             }
+
+            if (!$c1v["lerp"] || !$c2v["lerp"] || !$c3v["lerp"]) {
+                continue;
+            }
+
+            $v1 = $c1v["rgb"];
+            $v2 = $c2v["rgb"];
+            $v3 = $c3v["rgb"];
 
             $vl = array(
                 floor(($v1[0] + $v2[0]) / 2),
